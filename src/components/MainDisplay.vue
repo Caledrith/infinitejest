ab4f298d3f5d0cb153fdee5102611b9ce26ab344
samosbor <samosbor@byu.edu>
2019-10-28 18:32:08 -0600

    joke list in there

--- src/components/MainDisplay.vue
+++ src/components/MainDisplay.vue
@@ -5,7 +5,14 @@
       wrap
     >
       <v-list>
-
+        <v-list-item
+          v-for="(joke, i) in jokes"
+          :key="i"
+        >
+          <v-list-item-content>
+              <v-list-item-title v-text="joke"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-layout>
   </v-container>
@@ -17,12 +24,13 @@ export default {
     jokes: []
   }),
   created() {
-    this.$http.get('http://api.openweathermap.org/data/2.5/find?q=stockholm&type=like&appid=766b78c39446a8fa6313c3b7b2063ade').then(function(response){
-      if(response.status == "200"){
-        console.log(response);
-        this.jokes = response.data.list[0]
-      }
-    })
+    // this.$http.get('localhost:8080/getjokes').then(function(response){
+    //   if(response.status == "200"){
+    //     console.log(response);
+    //     this.jokes = response.data.list[0]
+    //   }
+    // })
+    this.jokes = ["this is a joke lol", "another one haha", "fgajkrbeniugbaiubgiuhawiufnbiuer long joke bndsakjhgbrhijabghibehwabghbarehgb"]
   },
   methods: {
 
