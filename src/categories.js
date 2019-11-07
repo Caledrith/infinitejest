9a9d9c838cc5f57052136bc591186a0ccb614289
DallinSharp <sharp.dallin@gmail.com>
2019-11-06 18:17:40 -0700

    Here you go, Sam

---
+++ src/categories.js
@@ -0,0 +1,9 @@
+export var testCategories = 
+[
+    {id: 0, categoryName: "All"}, 
+    {id: 1, categoryName: "One-Liners"}, 
+    {id: 2, categoryName: "Puns & Dad Jokes"}, 
+    {id: 3, categoryName: "Laffy Taffy Riddles"}, 
+    {id: 4, categoryName: "Tricky Riddles"}, 
+    {id: 5, categoryName: "Shaggy Dog Stories"}
+];
